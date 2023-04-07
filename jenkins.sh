36b73254e859e40634b1d9e01717ddeecaaf7277
remzysolutions <125318818+remzysolutions@users.noreply.github.com>
2023-04-06 20:12:39 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,13 +2,13 @@
 
 sudo apt-get update
 sudo apt-get install default-jdk -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo apt install maven -y
 

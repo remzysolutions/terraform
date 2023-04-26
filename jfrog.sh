ca4650de55456c56e7fbc77ab3cf8dd64e000c6d
remzysolutions <125318818+remzysolutions@users.noreply.github.com>
2023-04-26 13:19:06 -0400

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt dist-upgrade
 sudo apt autoremove
 sudo apt update
-sudo apt-get install openjdk-8-jdk openjdk-8-doc
+sudo apt-get install openjdk-8-jdk openjdk-8-doc -y
 echo "deb https://releases.jfrog.io/artifactory/artifactory-debs bionic main" | sudo tee -a /etc/apt/sources.list.d/jfrog.list
 wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
 sudo apt update
-sudo apt install jfrog-artifactory-oss
+sudo apt install jfrog-artifactory-oss -y
 sudo systemctl start artifactory
 sudo systemctl enable artifactory
 sudo systemctl status artifactory

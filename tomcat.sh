9eb8e75a8f03310ba675922d0548bc20b107a4c8
remzysolutions <125318818+remzysolutions@users.noreply.github.com>
2023-04-28 19:15:15 -0400

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -6,16 +6,8 @@
   sudo apt-get install tomcat9-docs tomcat9-examples tomcat9-admin -y
   sudo cp -r /usr/share/tomcat9-admin/* /var/lib/tomcat9/webapps/ -v
 
-  sudo bash -c 'cat > /etc/tomcat9/tomcat-users.xml <<EOF
-<?xml version="1.0" encoding="UTF-9"?>
-<tomcat-users xmlns="http://tomcat.apache.org/xml"
-              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-              xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
-              version="1.0">
-  <role rolename="manager-script"/>
-  <user username="tomcat" password="password" roles="manager-script"/>
-</tomcat-users>
-EOF'
+  echo "<role rolename="manager-script"/>" | sudo tee -a /etc/tomcat9/tomcat-users.xml
+  echo "<user username="tomcat" password="password" roles="manager-script"/>" | sudo tee -a /etc/tomcat9/tomcat-users.xml
 
 # Restart Tomcat service
 sudo systemctl restart tomcat9

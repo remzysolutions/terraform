701d559887bd8e51b51672ab5f18325527a4be04
remzysolutions <125318818+remzysolutions@users.noreply.github.com>
2023-04-28 20:06:52 -0400

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -6,8 +6,12 @@
   sudo apt-get install tomcat9-docs tomcat9-examples tomcat9-admin -y
   sudo cp -r /usr/share/tomcat9-admin/* /var/lib/tomcat9/webapps/ -v
 
-  echo "<role rolename="manager-script"/>" | sudo tee -a /etc/tomcat9/tomcat-users.xml
-  echo "<user username="tomcat" password="password" roles="manager-script"/>" | sudo tee -a /etc/tomcat9/tomcat-users.xml
+  # Define the new roles and user
+NEW_ROLES="<role rolename=\"manager\"/>\n  <role rolename=\"manager-gui\"/>\n  <role rolename=\"manager-script\"/>\n  <role rolename=\"manager-status\"/>"
+NEW_USER="<user username=\"tomcat\" password=\"password\" roles=\"manager,manager-gui,manager-script,manager-status\"/>"
+
+# Add the new roles and user to the tomcat-users.xml file
+sudo sed -i "/<\/tomcat-users>/i \ \ $NEW_ROLES\n\n  $NEW_USER" /etc/tomcat9/tomcat-users.xml
 
 # Restart Tomcat service
 sudo systemctl restart tomcat9
